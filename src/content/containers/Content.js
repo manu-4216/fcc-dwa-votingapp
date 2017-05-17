c50329faa65c5a1451442a8b4aad7f1e88e4e0f2
Manuel Micu <manu4216@gmail.com>
2017-05-17 02:16:52 +0200

    Add basic FAB button for adding polls

--- src/content/containers/Content.js
+++ src/content/containers/Content.js
@@ -1,14 +1,23 @@
 var React = require('react');
 //var PollList = require('../components/Content0');
 var axios = require('axios');
+var FloatingActionButton = require('fab/containers//FloatingActionButton');
 
 class PollListContainer extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            polls: []
+            polls: [],
+            addPollOpen: false
         }
+        this.displayAddPollForm = this.displayAddPollForm.bind(this);
+    }
+
+    displayAddPollForm () {
+        this.setState({
+            addPollOpen: true
+        })
     }
 
     componentDidMount() {
@@ -30,7 +39,18 @@ class PollListContainer extends React.Component {
     render() {
         //return <PollList polls={this.state.polls} />;
         //return <div>{this.state.polls}</div>
-        return <div>LIST OF POLLS</div>
+        return (
+            <div>
+                <div>LIST OF POLLS</div>
+                <FloatingActionButton
+                    handleClick={this.displayAddPollForm}
+                />
+                {this.state.addPollOpen ?
+                    <div>OPENED FORM</div> :
+                    <div>CLOSED FORM</div>
+                }
+            </div>
+        )
     }
 }
 
---
+++ src/fab/containers/FloatingActionButton.js
@@ -0,0 +1,23 @@
+var React = require('react');
+require('../style/main.scss');
+
+class FloatingActionButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className='add-button magic-circle pulse' onClick={this.props.handleClick} >
+  				<span className='add-button--text'>+</span>
+			</div>
+        )
+    }
+}
+
+module.exports = FloatingActionButton
