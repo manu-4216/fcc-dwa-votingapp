d084bed038fdf73639bd2984ee513b22e790f15e
Manuel Micu <manu4216@gmail.com>
2020-12-27 22:06:54 +0000

    Update versions

--- routes/index.js
+++ routes/index.js
@@ -3,97 +3,91 @@ var User = require('../models/users.js');
 var PollHandler = require('../controllers/pollHandler.server.js');
 
 module.exports = function (app, passport) {
-	//'use strict';
+  //'use strict';
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/');
-		}
-	}
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect('/');
+    }
+  }
 
-	var pollHandler = new PollHandler();
+  var pollHandler = new PollHandler();
 
-	app.route('/poll/:id')
-		.get(function(req, res) {
-			res.sendFile(path + '/index.html');
-		})
+  app.route('/poll/:id').get(function (req, res) {
+    res.sendFile(path + '/index.html');
+  });
 
-	app.route('/')
-		.get(function(req, res) {
-			res.sendFile(path + '/index.html');
-		})
+  app.route('/').get(function (req, res) {
+    res.sendFile(path + '/index.html');
+  });
 
+  app.route('/checklogin').get(function (req, res) {
+    res.send({ logged: req.isAuthenticated() });
+  });
 
-	app.route('/checklogin')
-		.get(function (req, res) {
-			res.send({ logged: req.isAuthenticated() })
-		})
+  app
+    .route('/login')
+    .post(passport.authenticate('local'), function (req, res) {
+      res.send({ logged: true });
+    })
+    .get(isLoggedIn, function (req, res) {
+      res.redirect('/');
+    });
 
-	app.route('/login')
-		.post(passport.authenticate('local'), function(req, res) {
-			res.send({ logged: true });
-		 })
-		 .get(isLoggedIn, function(req, res) {
-	 		res.redirect('/')
-	 	 });
+  app.route('/register').post(function (req, res, next) {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function (err) {
+      if (err) {
+        console.log('error while user register!', err);
+        return next(err);
+      }
+      passport.authenticate('local');
 
-	app.route('/register')
-		.post(function(req, res, next) {
-		  var newUser = new User({ username: req.body.username });
-		  User.register(newUser, req.body.password, function(err) {
-			if (err) { console.log('error while user register!', err); return next(err); }
-				passport.authenticate('local');
+      res.send({ logged: true });
+    });
+  });
 
-				res.send({ logged: true });
-		  });
-		});
+  app.route('/logout').get(function (req, res) {
+    req.logout();
+    res.send({ logged: false });
+  });
 
+  app.route('/api/:id').get(isLoggedIn, function (req, res) {
+    res.json(req.user.github);
+  });
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.send({ logged: false });
-		});
+  app.route('/api/addpoll').post(isLoggedIn, pollHandler.addPoll);
 
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
+  app.route('/api/vote').post(pollHandler.vote);
+  //////////////
 
-	app.route('/api/addpoll')
-		.post(isLoggedIn, pollHandler.addPoll)
+  app
+    .route('/api/poll/:id')
+    .get(pollHandler.getPoll)
+    .delete(pollHandler.deletePoll);
 
+  app
+    .route('/api/polls')
+    //.post(isLoggedIn, pollHandler.getAllPolls)
+    .post(function (req, res) {
+      console.log(
+        'The user is ' + req.isAuthenticated() ? '' : 'not' + ' authenticated'
+      );
 
-	app.route('/api/vote')
-		.post(pollHandler.vote)
-//////////////
+      if (req.isAuthenticated()) {
+        if (req.body.filter === 'all') {
+          pollHandler.getAllPolls(req, res);
+        } else {
+          pollHandler.getAllUserPolls(req, res);
+        }
+      } else {
+        pollHandler.getAllPolls(req, res);
+      }
+    });
 
-	app.route('/api/poll/:id')
-		.get(pollHandler.getPoll)
-		.delete(pollHandler.deletePoll)
-
-	app.route('/api/polls')
-		//.post(isLoggedIn, pollHandler.getAllPolls)
-		.post(function(req, res) {
-			console.log('The use is ' + req.isAuthenticated() ? '' : 'not' + ' authenticated');
-
-			if (req.isAuthenticated()) {
-				if (req.body.filter === 'all') {
-					pollHandler.getAllPolls(req, res);
-				} else {
-					pollHandler.getAllUserPolls(req, res);
-				}
-
-			} else {
-				pollHandler.getAllPolls(req, res)
-			}
-		})
-
-	app.route(['/polls', '/all'])
-		.get(function(req, res) {
-		   res.redirect('/')
-		});
-
+  app.route(['/polls', '/all']).get(function (req, res) {
+    res.redirect('/');
+  });
 };
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-////'use strict';
-
 var express = require('express');
 var routes = require('./routes/index.js');
 var mongoose = require('mongoose');
@@ -13,36 +11,36 @@ require('./config/passport')(passport);
 app.set('port', process.env.PORT || 3000);
 
 // Configure the db:
-//mongoose.connect(process.env.MONGO_URI);
-mongoose.connect(process.env.DB_URI);
-//mongoose.connect("mongodb://127.0.0.1:27017/votingapp");
+console.log(process.env.DB_URI);
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 // Use body parser:
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 //app.use(express.static(process.cwd()+ '/public'));
 app.use('/', express.static(process.cwd() + '/'));
 app.use('/poll', express.static(process.cwd() + '/'));
 
 // Middleware for logging all the requests:
-function logger (req, res, next) {
-    console.log(req.method + ' : ' +  req.originalUrl);
-    next()
+function logger(req, res, next) {
+  console.log(req.method + ' : ' + req.originalUrl);
+  next();
 }
-app.use(logger)
+app.use(logger);
 
-
-app.use(session({
-	secret: 'sesame',
-	resave: false,
-	saveUninitialized: true
-}));
-
+app.use(
+  session({
+    secret: 'sesame',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Passport
 app.use(passport.initialize());
@@ -51,6 +49,6 @@ app.use(passport.session());
 routes(app, passport);
 
 var port = app.get('port');
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+app.listen(port, function () {
+  console.log('Node.js listening on port ' + port + '...');
 });
